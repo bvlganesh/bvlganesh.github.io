87537e1399bd68e2c7bd744fea04838f57a3855c
Vamsi Vattumilli <vamsi.vattumilli@gmail.com>
2024-08-18 23:55:41 +0530

    Changes by Vamsi

--- lwd/lwd2024/source/src/lwd.js
+++ lwd/lwd2024/source/src/lwd.js
@@ -1,76 +1,41 @@
 // src/Carousel.js
 
 import React, { useState, useEffect } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import './lwd.css'; // Optional: You can style your carousel in Carousel.css
 
-const Lwd = ({ images, interval = 15000 }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [nextImageIndex, setNextImageIndex] = useState(1);
-  const [progressWidth, setProgressWidth] = useState(0);
-  const [isTransitioning, setIsTransitioning] = useState(false);
+const Lwd = ({ images }) => {
 
-  // Function to go to the next slide
-  const nextSlide = () => {
-    if (!isTransitioning) {
-      setIsTransitioning(true);
-      setTimeout(() => {
-        const newIndex = (currentImageIndex + 1) % images.length;
-        setCurrentImageIndex(newIndex);
-        setNextImageIndex((newIndex + 1) % images.length);
-        setProgressWidth(0); // Reset progress bar width
-        setIsTransitioning(false);
-      }, 300); // 300ms should match CSS transition duration
-    }
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+      setIndex(selectedIndex);
   };
-
-  // Function to go to the previous slide
-  const prevSlide = () => {
-    if (!isTransitioning) {
-      setIsTransitioning(true);
-      setTimeout(() => {
-        const newIndex = (currentImageIndex - 1 + images.length) % images.length;
-        setCurrentImageIndex(newIndex);
-        setNextImageIndex((newIndex + 1) % images.length);
-        setProgressWidth(0); // Reset progress bar width
-        setIsTransitioning(false);
-      }, 300); // 300ms should match CSS transition duration
+  
+  const onSlide = (eventKey, direction) => {
+    if(eventKey == 0){
+      console.log("First");
     }
-  };
 
-  // Automatically move to the next slide at a specified interval
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      nextSlide();
-    }, interval);
-
-    // Update progress bar width every 100ms
-    const progressIntervalId = setInterval(() => {
-      setProgressWidth((prevWidth) => {
-        const increment = 100 / (interval / 100); // Increase by 100ms
-        return Math.min(prevWidth + increment, 100); // Cap at 100% width
-      });
-    }, 100);
-
-    return () => {
-      clearInterval(intervalId);
-      clearInterval(progressIntervalId);
-    };
-  }, [currentImageIndex, interval]);
+    if(eventKey == 28) {
+      console.log("Last");
+    }
+  }
 
   return (
-    <div className="carousel">
-      <div className={`slide ${isTransitioning ? 'transitioning' : ''}`}>
-        <img src={process.env.PUBLIC_URL + images[currentImageIndex]} alt="carousel slide"/>
-      </div>
-      <div className={`slide ${isTransitioning ? 'transitioning' : ''}`}>
-        <img src={process.env.PUBLIC_URL + images[nextImageIndex]} alt="carousel slide" />
-      </div>
-      <div className="timer-bar-container">
-        <div className="timer-bar" style={{ width: `${progressWidth}%` }} />
-      </div>
-      <button className="prev-button" onClick={prevSlide}>&lt;</button>
-      <button className="next-button" onClick={nextSlide}>&gt;</button>
-    </div>
+    <Carousel indicators={false} nextLabel={`Keep Reading`} prevLabel={`Missed Last one?`} onSlide={onSlide}
+      activeIndex={index} onSelect={handleSelect} controls={index == 28 ? false : true}>
+      {images.map((image, index) => (
+        <Carousel.Item interval={5000}>
+          <div className="box">
+            <img
+              className="d-block box-img"
+              src={image}
+            />
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
 
